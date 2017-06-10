0b0d4e4caac55baa7b0b261b6c149480d67bd69f
Emmanuel Quagraine <eman_the_man@hotmail.com>
2017-06-10 14:05:47 -0600

    seeding revised and readme update` `

---
+++ DrafterDatabase/seeding/types.PostDeployment.sql
@@ -0,0 +1,50 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+-- the commented code below is an example of seeding. We're not going to execute this particular part because...
+-- the types will be part of drafter-core as enums.
+/*
+MERGE INTO [dbo].[transaction_types] AS [Target]
+USING (
+VALUES (1, N'eliminated', N'Eliminated', N'2017-03-14 00:30:38'), (2, N'revived', N'Revive', N'2017-03-14 00:30:55')
+)
+AS [Source] ([id], [name], [description], [add_dt])
+ON [Target].[id] = [Source].[id]
+WHEN MATCHED THEN
+UPDATE SET [name] = [Source].[name],
+[description] = [Source].[description]
+WHEN NOT MATCHED BY TARGET THEN
+INSERT ([id], [name], [description], [add_dt])
+VALUES ([id], [name], [description], [add_dt]);
+*/
+
+MERGE INTO [dbo].[user_permission_types] AS [Target]
+USING (
+VALUES 
+(1, N'creator', N'Someone who can create', 1, 1, 1, 1), 
+(2, N'editor', N'Someone who can edit', 1, 0, 1, 0),
+(3, N'analysis', N'Someone who can get access to all the crazy analytical data', 0, 0, 0, 1)
+)
+AS [Source] ([id], [name], [description], [can_edit], [can_delete], [can_read], [can_create])
+ON [Target].[id] = [Source].[id]
+WHEN MATCHED THEN
+UPDATE SET 
+[name] = [Source].[name],
+[description] = [Source].[description],
+[can_edit] = [Source].[can_edit],
+[can_delete] = [Source].[can_delete],
+[can_read] = [Source].[can_read],
+[can_create] = [Source].[can_create]
+WHEN NOT MATCHED BY TARGET THEN
+INSERT ([id], [name], [description], [can_edit], [can_delete], [can_read], [can_create])
+VALUES ([id], [name], [description], [can_edit], [can_delete], [can_read], [can_create]);
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # drafter-sql
+
 The database for project drafter
+
+## Requirements
+
+1. SQL Server 2014 or 2016
+1. Visual Studio
+
+## Local Instance Setup
+
+1. Open project in Visual Studio
+1. Publish Database (you can use `DrafterDatabase.publish.xml`)
+    * at this point, your local database should be created
+    * the database should also have the seeded data
+1. Open up SQL Server Object Explorer in Visual Studio
+1. Navigate to `SQLServer > (localdb)\MSSQLLocalDB` (or whatever server you deployed the database to)
+
+Query away!
+
+## Seeding
+
+It's nice to have some data to work with. Create some post deployment scripts in the `seeding` folder.
+
+post deployment scripts can be created by adding a Post-Deployment Script item in the seeding folder (`Add > New Item... > SQLServer > User Scripts > Post-Deployment Script`)
